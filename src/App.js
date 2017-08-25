e36add55dde9c74b1f0acb85e5bb5a20477a6127
Steven Worley <bitcopia@gmail.com>
2017-08-24 23:35:24 -0700

    connect to websocket, set states, visualize some primitive data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unconfirmedTxs: [],
+      blocks: [],
+      unconfirmedTxSize: 0,
+      currentBlockIndex: 0
+    };
+  }
+
+  componentDidMount(){
+    var i = 0
+    this.connection = new WebSocket('wss://ws.blockchain.info/inv');
+    this.connection.onmessage = evt => { 
+      var msgObj = JSON.parse(evt.data)
+      // use to debug block data
+      // if (i > 10) {
+      //   this.connection.send(JSON.stringify({"op":"ping_block"}));
+      //   i = 0
+      // }
+      // i += 1
+      if (msgObj.op === 'utx') {
+        this.setState({
+          unconfirmedTxs: this.state.unconfirmedTxs.concat([msgObj]),
+          unconfirmedTxSize: this.state.unconfirmedTxSize + msgObj.x.size
+        })
+      }
+      else if (msgObj.op === 'block'){
+        this.setState({
+          unconfirmedTxs: [],
+          blocks: this.state.blocks.concat([msgObj]),
+          unconfirmedTxSize: 0,
+          currentBlockIndex: msgObj.x.blockIndex
+        })
+      }
+    };
+    
+    this.connection.onopen = () => {
+      // subscribe to unconfirmed transactions
+      this.connection.send(JSON.stringify({"op":"unconfirmed_sub"}));
+      // subscribe to blocks
+      this.connection.send(JSON.stringify({"op":"blocks_sub"}));
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +55,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="unconf-tx-size">
+          <p>Total Size of Unconfirmed Transactions:</p>
+          <p>{this.state.unconfirmedTxSize}</p>
+          {/* 
+            create ul of last 5 UTXs 
+            <ul>{ this.state.unconfirmedTxs.slice(-5).map( (msg, idx) => <li key={'msg-' + idx }>{ msg }</li> )}</ul> 
+          */}
+        </div>
+        <div className="current-block-index">
+          <p>Current Block Index:</p>
+          <p>{this.state.currentBlockIndex}</p>
+        </div>
       </div>
     );
   }
