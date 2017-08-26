3ad0bcc625fcb2d4b4f582613b0ebb4f1d31ed41
Steven Worley <bitcopia@gmail.com>
2017-08-25 17:29:07 -0700

    add new states for transaction sizes and change index to height

--- src/App.js
+++ src/App.js
@@ -8,9 +8,33 @@ class App extends Component {
     this.state = {
       unconfirmedTxs: [],
       blocks: [],
-      unconfirmedTxSize: 0,
-      currentBlockIndex: 0
+      unconfirmedTxSizes: [],
+      totalUnconfirmedTxSize: 0,
+      currentBlockHeight: 0,
+      avgUnconfTxSize: 0,
+      maxUnconfTxSize: 0,
+      minUnconfTxSize: 10000000
     };
+  }
+
+  getAvgTxSize(txs) {
+    return this.state.totalUnconfirmedTxSize / txs.length
+  }
+
+  getMaxTxSize(txSize) {
+    if (this.state.maxUnconfTxSize < txSize) {
+      this.setState({
+        maxUnconfTxSize: txSize
+      })
+    }
+  }
+
+  getMinTxSize(txSize) {
+    if (this.state.minUnconfTxSize > txSize) {
+      this.setState({
+        minUnconfTxSize: txSize
+      })
+    }
   }
 
   componentDidMount(){
@@ -25,17 +49,23 @@ class App extends Component {
       // }
       // i += 1
       if (msgObj.op === 'utx') {
+        var txSize = msgObj.x.size
+        this.getMaxTxSize(txSize)
+        this.getMinTxSize(txSize)
         this.setState({
-          unconfirmedTxs: this.state.unconfirmedTxs.concat([msgObj]),
-          unconfirmedTxSize: this.state.unconfirmedTxSize + msgObj.x.size
+          // unconfirmedTxs: this.state.unconfirmedTxs.concat([msgObj]),
+          unconfirmedTxs: [msgObj].concat(this.state.unconfirmedTxs),
+          totalUnconfirmedTxSize: this.state.totalUnconfirmedTxSize + txSize,
+          unconfirmedTxSizes: ([txSize].concat(this.state.unconfirmedTxSizes)).reverse(),
+          avgUnconfTxSize: this.getAvgTxSize(this.state.unconfirmedTxSizes)
         })
       }
       else if (msgObj.op === 'block'){
         this.setState({
           unconfirmedTxs: [],
           blocks: this.state.blocks.concat([msgObj]),
-          unconfirmedTxSize: 0,
-          currentBlockIndex: msgObj.x.blockIndex
+          totalUnconfirmedTxSize: 0,
+          currentBlockHeight: msgObj.x.height
         })
       }
     };
@@ -43,6 +73,8 @@ class App extends Component {
     this.connection.onopen = () => {
       // subscribe to unconfirmed transactions
       this.connection.send(JSON.stringify({"op":"unconfirmed_sub"}));
+      // get last block data
+      this.connection.send(JSON.stringify({"op":"ping_block"}));
       // subscribe to blocks
       this.connection.send(JSON.stringify({"op":"blocks_sub"}));
     }
@@ -55,18 +87,38 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <div className="unconf-tx-size">
-          <p>Total Size of Unconfirmed Transactions:</p>
-          <p>{this.state.unconfirmedTxSize}</p>
-          {/* 
-            create ul of last 5 UTXs 
-            <ul>{ this.state.unconfirmedTxs.slice(-5).map( (msg, idx) => <li key={'msg-' + idx }>{ msg }</li> )}</ul> 
-          */}
+        <div className="container">
+          <div className="current-block-height">
+            <p>Current Block Height:</p>
+            <p>{this.state.currentBlockHeight}</p>
+          </div>
+          <div className="total-unconf-tx-size">
+            <p>Total Size of Unconfirmed Transactions:</p>
+            <p>{this.state.totalUnconfirmedTxSize}</p>
+          </div>
+        {/*  <div className="unconf-tx-sizes">
+            <p>Transaction Sizes:</p>
+            <p>{this.state.unconfirmedTxSizes}</p>
+          </div>
+        */}
+          <div className="avg-unconf-tx-size">
+            <p>Average Unconfirmed Transaction Size:</p>
+            <p>{this.state.avgUnconfTxSize}</p>
+          </div>
+          <div className="max-unconf-tx-size">
+            <p>Maximum Unconfirmed Transaction Size:</p>
+            <p>{this.state.maxUnconfTxSize}</p>
+          </div>
+          <div className="max-unconf-tx-size">
+            <p>Minimum Unconfirmed Transaction Size:</p>
+            <p>{this.state.minUnconfTxSize}</p>
+          </div>
+
+
         </div>
-        <div className="current-block-index">
-          <p>Current Block Index:</p>
-          <p>{this.state.currentBlockIndex}</p>
-        </div>
+        {/*
+        <ul>{ this.state.unconfirmedTxs.slice().map( (msg, idx) => <li key={'msg-' + idx }>{ JSON.stringify(msg) }</li> )}</ul> 
+        */}
       </div>
     );
   }
