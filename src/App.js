f66ce843a0d0c77c8aba0e1d550b9c63e3b056a6
Steven Worley <bitcopia@gmail.com>
2017-08-31 23:00:33 -0700

    add new functions for mempool

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import request from 'superagent';
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       unconfirmedTxs: [],
       blocks: [],
@@ -13,15 +14,37 @@ class App extends Component {
       currentBlockHeight: 0,
       avgUnconfTxSize: 0,
       maxUnconfTxSize: 0,
-      minUnconfTxSize: 10000000
-    };
+      minUnconfTxSize: 10000000,
+      mempoolSizeInMb: 0,
+      mempoolTxCount: 0
+    }
+  }
+
+  fetchMempoolSize() {
+    request.get('https://blockchain.info/charts/mempool-size?format=json&cors=true')
+      .end(function(err, response){
+        var array = response.body.values
+        var size = array[array.length - 1].y
+        var sizeInMb = size / 1000000
+        return sizeInMb
+      })
+  }
+
+  fetchMempoolTxCount() {
+    request.get('https://blockchain.info/charts/mempool-count?format=json&cors=true')
+      .end(function(err, response){
+        var array = response.body.values
+        var txCount = array[array.length - 1].y
+        return txCount
+      })
   }
 
   getAvgTxSize(txs) {
-    return this.state.totalUnconfirmedTxSize / txs.length
+    var avg = this.state.totalUnconfirmedTxSize / txs.length
+    return Math.round(avg)
   }
 
-  getMaxTxSize(txSize) {
+  setMaxTxSize(txSize) {
     if (this.state.maxUnconfTxSize < txSize) {
       this.setState({
         maxUnconfTxSize: txSize
@@ -29,7 +52,7 @@ class App extends Component {
     }
   }
 
-  getMinTxSize(txSize) {
+  setMinTxSize(txSize) {
     if (this.state.minUnconfTxSize > txSize) {
       this.setState({
         minUnconfTxSize: txSize
@@ -39,21 +62,20 @@ class App extends Component {
 
   componentDidMount(){
     var i = 0
-    this.connection = new WebSocket('wss://ws.blockchain.info/inv');
+    this.connection = new WebSocket('wss://ws.blockchain.info/inv')
     this.connection.onmessage = evt => { 
       var msgObj = JSON.parse(evt.data)
       // use to debug block data
       // if (i > 10) {
-      //   this.connection.send(JSON.stringify({"op":"ping_block"}));
+      //   this.connection.send(JSON.stringify({"op":"ping_block"}))
       //   i = 0
       // }
       // i += 1
       if (msgObj.op === 'utx') {
         var txSize = msgObj.x.size
-        this.getMaxTxSize(txSize)
-        this.getMinTxSize(txSize)
+        this.setMaxTxSize(txSize)
+        this.setMinTxSize(txSize)
         this.setState({
-          // unconfirmedTxs: this.state.unconfirmedTxs.concat([msgObj]),
           unconfirmedTxs: [msgObj].concat(this.state.unconfirmedTxs),
           totalUnconfirmedTxSize: this.state.totalUnconfirmedTxSize + txSize,
           unconfirmedTxSizes: ([txSize].concat(this.state.unconfirmedTxSizes)).reverse(),
@@ -61,22 +83,30 @@ class App extends Component {
         })
       }
       else if (msgObj.op === 'block'){
+        console.log('pool:' + msgObj.x.foundBy.description)
+        console.log('totalBTCSent:' + msgObj.x.totalBTCSent / 100000000)
+        console.log('#tx:' + msgObj.x.nTx)
+        console.log('blockSize:' + msgObj.x.size)
         this.setState({
+          // TODO reset other states
           unconfirmedTxs: [],
           blocks: this.state.blocks.concat([msgObj]),
           totalUnconfirmedTxSize: 0,
           currentBlockHeight: msgObj.x.height
         })
       }
-    };
+    }
     
     this.connection.onopen = () => {
+      // TODO set state with these functions
+      this.fetchMempoolSize()
+      this.fetchMempoolTxCount()
       // subscribe to unconfirmed transactions
-      this.connection.send(JSON.stringify({"op":"unconfirmed_sub"}));
+      this.connection.send(JSON.stringify({"op":"unconfirmed_sub"}))
       // get last block data
-      this.connection.send(JSON.stringify({"op":"ping_block"}));
+      this.connection.send(JSON.stringify({"op":"ping_block"}))
       // subscribe to blocks
-      this.connection.send(JSON.stringify({"op":"blocks_sub"}));
+      this.connection.send(JSON.stringify({"op":"blocks_sub"}))
     }
   }
 
@@ -113,6 +143,10 @@ class App extends Component {
             <p>Minimum Unconfirmed Transaction Size:</p>
             <p>{this.state.minUnconfTxSize}</p>
           </div>
+          <div className="mempool-size">
+            <p>Mempool Size:</p>
+            <p>{this.state.mempoolSizeInMb}</p>
+          </div>
 
 
         </div>
@@ -120,8 +154,8 @@ class App extends Component {
         <ul>{ this.state.unconfirmedTxs.slice().map( (msg, idx) => <li key={'msg-' + idx }>{ JSON.stringify(msg) }</li> )}</ul> 
         */}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
