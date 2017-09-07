a521de6468248003044be29458bdab181702e16a
Steven Worley <bitcopia@gmail.com>
2017-09-06 23:29:03 -0700

    move components to folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Anime from 'react-anime';
-import logo from './logo.svg'
-import './App.css'
+import logo from '../logo.svg'
+import '../App.css'
 
 class App extends Component {
   constructor() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Mempool from './Mempool'
+import App from './components/App';
+import Mempool from './components/Mempool'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
